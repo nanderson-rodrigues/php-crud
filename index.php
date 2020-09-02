3c51e08071fe7fbc8ee53554fac4f0e5866094cf
Nanderson Rodrigues <nandersondsr@gmail.com>
2020-09-02 16:08:06 -0300

    adiconar index page e style

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset=utf-8>
+	<meta name=description content="">
+	<meta name=viewport content="width=device-width, initial-scale=1">
+	<title>PHP CRUD - BOOKS</title>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+
+	<main>
+	
+	<div class="container text-center">
+		<h1 class="py-4"><i class="large material-icons">library_books</i> Book Store</h1>
+
+		<div class="d-flex justify-content-center">
+
+			<form action="" method="post" accept-charset="utf-8">
+
+				<div class="py-2">
+					<div class="input-group mb-2">
+
+        				<div class="input-group-prepend">
+         					<div class="input-group-text bg-warning"><i class="large material-icons">perm_identity</i></div>
+    					</div>
+
+        				<input type="text" class="form-control" id="inlineFormInputGroup" autocomplete="off" placeholder="Id">
+
+      				</div>
+				</div>
+
+				<div class="pt-2">
+					<div class="input-group mb-2">
+
+        				<div class="input-group-prepend">
+         					<div class="input-group-text bg-warning"><i class="large material-icons">perm_identity</i></div>
+    					</div>
+
+        				<input type="text" class="form-control" id="inlineFormInputGroup" autocomplete="off" placeholder="Id">
+
+      				</div>
+				</div>
+
+			</form>	
+
+		</div>
+	</div>
+
+	</main>
+
+
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+</html>

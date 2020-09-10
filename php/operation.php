033138c484ecd9b8802e9ce95fb452cac4c00d12
Nanderson Rodrigues <nandersondsr@gmail.com>
2020-09-09 21:27:21 -0300

    adicionar funcionalidade de inserir dados

--- php/operation.php
+++ php/operation.php
@@ -10,3 +10,42 @@ if (isset($_POST['create'])) {
 	createData();
 }
 
+function createData() {
+	$book_name = textboxValue("book_name");
+	$book_publisher = textboxValue("book_publisher");
+	$book_price = textboxValue("book_price");
+
+	if ( $book_name && $book_publisher && $book_price) {
+
+		$sql = "
+			INSERT INTO books( book_name, book_publisher, book_price )
+			VALUES ('$book_name', '$book_publisher', '$book_price')
+		";
+
+		if (mysqli_query($GLOBALS['con'], $sql)) {
+			textMessage("success", "Successfully inserted!!!");
+		}else {
+			echo "Error !!!";
+		}
+
+	}else {
+		textMessage("error", "Preecha o formulário!");
+	}
+}
+
+function textboxValue($value) {
+	//trim() -> security for sql injection
+	$textbox = mysqli_real_escape_string($GLOBALS['con'], trim($_POST[$value])); 
+	if (empty($textbox)) {
+		return false;
+	}
+	else {
+		return $textbox;
+	}
+}
+
+//Messages
+function textMessage($classname, $msg) {
+	$element = 	"<h6 class='$classname'>$msg</h6>";
+	echo $element;
+}
